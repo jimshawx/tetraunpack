1609e8c014270ad0927d460a98293e68b65732d0
Jim <jimshaw@iinet.net.au>
2021-06-17 17:43:30 +0100

    fixed a few bugs in the unpack. went back to 68000 register names while debugging against Amiga. first few bytes of unpack are correct now (at least).

--- TetraUnpack.c
+++ TetraUnpack.c
@@ -1,3 +1,6 @@
+#include <assert.h>
+#include <windows.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -77,6 +80,8 @@ int main(int argc, char **argv)
 
 				if (hunk_type == 0x3E9)/* HUNK_CODE */
 				{
+					long t = ftell(f);
+					
 					unsigned int num_longs;
 					fread(&num_longs, 1, sizeof num_longs, f);
 					swap(&num_longs);
@@ -96,8 +101,8 @@ int main(int argc, char **argv)
 						//if (j != 0 && j % 8 == 0) fputc('\n', stdout);
 						//fprintf(stdout, "%04X %04X ", l >> 16, l & 0xffff);
 					}
-					dump_buffer(b, num_longs * 4);
-					disassemble_buffer(b, num_longs * 4);
+					//dump_buffer(b, num_longs * 4);
+					//disassemble_buffer(b, num_longs * 4);
 
 					if (!strncmp((char*)b+0xc4, " TETRAGON ", 10))
 						unpack(b);
@@ -147,113 +152,137 @@ int main(int argc, char **argv)
 }
 
 static unsigned char *A0, *A1, *A2, *A4;
-static unsigned int longbits, bitcnt, bits, D3, D4, D7;
+static unsigned int D0, D1, D2, D3, D4, D7;
 static unsigned char C,X;
 static void get_next_long_in_D0(void);
 static void get_next_D1_bits_in_D2(void);
-static void RLE(unsigned char *src);
-
+static void RLE(unsigned char *src, unsigned char *dst, unsigned int length);
+static unsigned int bswap(unsigned int s) { return (s >> 24) | (s << 24) | ((s >> 8) & 0xff00) | ((s << 8) & 0xff0000); }
 static void unpack(unsigned char *source)
 {
 	A1 = source + 0x100;//source data address which starts at 0x100 past start of code
-	A4 = 0x3e74a;//end address it's moved from the LEA of the RLE section.
+	A4 = 0x3e742;//unpack target address it's moved from the LEA of the RLE section.
 	A0 = A4;
-	A4 += 0x193b0;//source filesize - 256 : this number is plugged in
-	
-	if (A1 <= A4)
+	A0 += 0x193b0;//source filesize - 256, i.e. size of compressed data following the unpacker : this number is plugged in
+
+	//if (A1 <= A4)
 	{
 		A0 = A1;
-		A2 += 0x193b0;//filesize - 256, it's added from the A4+= ADDA.L instruction above
+		A0 += 0x193b0;//filesize - 256, it's added from the A4+= ADDA.L instruction above
 	}
-	else
-	{
-		do
-		{
-			*A4++ = *A1++;
-		} while (A0 < A4);
-	}
+	//else
+	//{
+	//	do
+	//	{
+	//		*A4++ = *A1++;
+	//	} while (A0 < A4);
+	//}
 	
 	A1 = 0x44941;//what? : this number is plugged in
-	A1 += (uintptr_t)A1;//0x89282 (561794)
-	
-	A0 -= 4; A2 = *(unsigned int *)A0;
-	A0 -= 4; longbits = *(unsigned int *)A0;
 
+	//A0 is pointing at end of packed data
+	
+	A0 -= 4; A2 = bswap(*(unsigned int *)A0);//A0 will be source+0x193b0+0x100, A2 is 1edb7
+	A2 += (uintptr_t)A1;//A2 is now 636f8
+	A0 -= 4; D0 = bswap(*(unsigned int *)A0);
+
+	//A0 is pointing at end of packed data
+	//A1 is pointing at the beginning of the unpack location for 1st step of compression
+	//A2 is pointing at end of unpack location
+	//A4 is pointing at start of unpack location
+
+	int unpacked_size = A2 - A4;
+	int unpacked_step1_size = A1 - A4;
+	A4 = malloc(unpacked_size);
+	A2 = A4 + unpacked_size;
+	A1 = A4 + unpacked_step1_size;
+	
+	//1st step of compression - looks like some kind of Huffman-style decompressor
+	
 	//0036
 	do
 	{
-		X = C = longbits & 1;
-		longbits >>= 1;
-		
-		if (longbits == 0) get_next_long_in_D0();
-
+		//0036
+		X = C = D0 & 1;
+		D0 >>= 1;
+		if (D0 == 0) get_next_long_in_D0();
 		if (C) goto iA8;
-		
-		bitcnt = 8;
+
+		D1 = 8;
+		D3 = 1;
+
+		//0042
+		X = C = D0 & 1;
+		D0 >>= 1;
+		if (D0 == 0) get_next_long_in_D0();
+		if (C) goto i86;
+
+		D1 = 3;
 		D4 = 0;
 
 		//004E
 		for (;;)
 		{
 			get_next_D1_bits_in_D2();
-			D3 = bits;
-			D3 += D4;
+			D3 = D2 + D4;
 			do
 			{
 				int count = 7;
 				//0056
 				do
 				{
-					X = C = longbits & 1;
-					longbits >>= 1;
+					X = C = D0 & 1;
+					D0 >>= 1;
 					
-					if (longbits == 0) get_next_long_in_D0();
+					if (D0 == 0) get_next_long_in_D0();
 
 					//roxl #1, D2
-					C = bits >> 31;
-					bits <<= 1;
-					bits |= X;
+					C = D2 >> 31;
+					D2 <<= 1;
+					D2 |= X;
 					X = C;
 
-				} while (--count);
+				} while (--count>-1);
+
+				//0062
+				*--A2 = D2;
 				
-				*--A2 = bits;
-				
-			} while (--D3);
+			} while ((int)--D3>-1);
 
 			goto i90;
 i6A:
-			bitcnt = 7;
+			D1 = 7;
 			D4 = 8;
 		}
 i7E:
-		bitcnt = 8;
-		D3 = bits + 2;
+		D1 = 8;
+		D3 = D2 + 2;
 i86:
 		get_next_D1_bits_in_D2();
 
 		//0088
 		do
 		{
-			const unsigned char tmp = *(A2 + bits - 1);
+			const unsigned char tmp = *(A2 + D2 - 1);
 			*--A2 = tmp;
-		} while (--D3);
+		} while ((int)--D3>-1);
 i90:;
-	} while (A2 < A1);
+	} while (A2 >= A1);
 
-	RLE(A1);
+	//0094
+	RLE(A1,A4, unpacked_size);
 	return;
 
 iA8:
-	bitcnt = 2;
+	D1 = 2;
 	get_next_D1_bits_in_D2();
-	if (bits < 2) goto i7E;
-	if (bits == 3) goto i6A;
+	if (D2 < 2) goto i7E;
+	if (D2 == 3) goto i6A;
 	
-	bitcnt = 8;
+	D1 = 8;
 	get_next_D1_bits_in_D2();
-	D3 = bits + 4;
-	bitcnt = 8;
+	D3 = D2 + 4;
+	D1 = 8;
 	
 	goto i86;
 }
@@ -265,11 +294,13 @@ iA8:
 //A0 - dest address
 //A1 - source address
 //A2 - end of source address
-static void RLE(unsigned char *src)
+static void RLE(unsigned char *src, unsigned char *dst, unsigned int length)
 {
-	unsigned char *dst/*A0*/ = 0x3e74a;//this number is plugged in
-	unsigned char *end/*A2*/ = 0x636F8;//this number is plugged in
+	//unsigned char *dst/*A0*/ = 0x3e742;//this number is plugged in
+	//unsigned char *end/*A2*/ = 0x636F8;//this number is plugged in
+	unsigned char *end = dst + length;
 
+	//00CE
 	do
 	{
 		unsigned char b = *src++;
@@ -290,39 +321,46 @@ static void RLE(unsigned char *src)
 
 		*dst++ = b;
 
-	} while (src < end);
+	} while (dst < end);
 
+	//00FA
 	//goto 0x3e742;//wow!	: this number is plugged in and is the entry point of the unpacked code
 }
 
 static void get_next_long_in_D0(void)
 {
-	A0 -= 4; longbits = *(unsigned int *)A0;
+	//70
+	A0 -= 4; D0 = bswap(*(unsigned int *)A0);
 
 	//move #$10,ccr (sets X, clears C)
 	//roxr.l #1, D0
-	C = X = longbits & 1;
-	longbits >>= 1;
-	longbits |= 0x80000000;//this bit is a marker, so when it gets shifted out D0 finally hits zero
+	C = X = D0 & 1;
+	D0 >>= 1;
+	D0 |= 0x80000000;//this bit is a marker, so when it gets shifted out D0 finally hits zero
+
+	char tt[16];
+	sprintf_s(tt, 10, "%08X\n", D0);
+	OutputDebugStringA(tt);
 }
 
 static void get_next_D1_bits_in_D2(void)
 {
-	bitcnt--;
-	bits = 0;
+	//0096
+	D1--;
+	D2 = 0;
 	
 	do
 	{
-		X = C = longbits & 1;
-		longbits >>= 1;
+		X = C = D0 & 1;
+		D0 >>= 1;
 
-		if (longbits == 0) get_next_long_in_D0();
+		if (D0 == 0) get_next_long_in_D0();
 
 		//roxl #1, D2
-		C = bits >> 31;
-		bits <<= 1;
-		bits |= X;
+		C = D2 >> 31;
+		D2 <<= 1;
+		D2 |= X;
 		X = C;
 		
-	} while (--bitcnt);
+	} while ((int)--D1>-1);
 }
